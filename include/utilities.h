54adb382b9729860bd00584a8a1d3a9dc78293da
Andrzej Hubka <92820312+Arnazell@users.noreply.github.com>
2025-01-20 17:25:24 +0100

    Update2 utilities.h

--- include/utilities.h
+++ include/utilities.h
@@ -5,7 +5,7 @@
 #include <iostream>
 
 // sciezka do pliku z magazynem
-#define WAREHOUSE_PATH "src/warehouse_data"
+#define WAREHOUSE_PATH "warehouse_data"
 
 // rodzaje bledow & wynikow
 #define IPC_RESULT_ERROR -1
